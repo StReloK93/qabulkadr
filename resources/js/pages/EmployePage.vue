aa7c6fd94ce40adb1bbc03c67aa84f75b464f0d8
StReloK93 <strelok0493@gmail.com>
2025-11-06 22:10:41 +0500

    fast

--- resources/js/pages/EmployePage.vue
+++ resources/js/pages/EmployePage.vue
@@ -1,10 +1,25 @@
 <template>
    <CrudPage>
       <template #column>
-         <Column body-style="text-align:center;width:10rem">
+         <Column>
             <template #body="{ data }">
-               {{ data.status.name }}
+               <span
+                  :class="[
+                     {'text-teal-700 bg-teal-100/70': data.status.id == 1},
+                     {'text-orange-700 bg-orange-100/70': data.status.id == 2},
+                     {'text-blue-700 bg-blue-100/70': data.status.id == 3},
+                     {'text-zinc-700 bg-zinc-100/70': data.status.id == 4},
+                  ]"
+                  class="px-3 rounded-full py-1"
+               >
+                  {{ data.status.name }}
+               </span>
                <!-- <Button icon="pi pi-trash" text rounded severity="danger" /> -->
+            </template>
+         </Column>
+         <Column>
+            <template #body="{ data }">
+               <Button icon="pi pi-eye" text rounded severity="success" />
             </template>
          </Column>
       </template>
@@ -14,7 +29,6 @@
       </template>
    </CrudPage>
 </template>
-
 <script setup lang="ts">
 import CrudPage from "./CrudPage.vue";
 </script>
