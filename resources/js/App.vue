8ace1ddb044ba0082063c3b97120c93097c76565
strelok93 <strelok0493@gmail.com>
2025-11-21 17:51:16 +0500

    as

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,8 @@
 <template>
-   <!-- <PrintQabulVaraqa /> -->
-   <ConfirmPopup class="print:hidden" />
+   <PrintQabulVaraqa class="bg-gray-200" />
+   <!-- <ConfirmPopup class="print:hidden" />
    <SplashScreen class="print:hidden" v-if="UserStore.initialized == false" />
-   <RouterView class="print:hidden h-dvh" v-else />
+   <RouterView class="print:hidden h-dvh" v-else /> -->
 </template>
 <script setup lang="ts">
 import SplashScreen from "@/components/BaseSplashScreen.vue";
--- resources/js/components/PrintQabulVaraqa.vue
+++ resources/js/components/PrintQabulVaraqa.vue
@@ -12,7 +12,78 @@
       </nav>
       <br /><br />
       <nav class="text-center text-[12pt] uppercase font-bold tracking-[4px] mb-1">Ariza</nav>
-      <p class="indent-14 text-[12pt]">Menga shu dasturni tezroq tugatishimga yordam qiling</p>
+      <p class="indent-14 text-[12pt]">
+         Meni 3-sonli Gidrometallurgiya zavodi Maydalash tsexiga Tsex boshlig'i lavozimiga ishga qabul
+         qilishingizni so'rayman
+      </p>
+      <div class="flex justify-between mt-6">
+         <span> «____»_____________ y. </span>
+         <section class="inline-flex flex-col items-center">
+            <span> _____________________ </span>
+            <span class="text-[8pt]"> (izmo)</span>
+         </section>
+      </div>
+      <div class="flex justify-between mt-8 text-[11pt]">
+         <span>Ijrochi: No'monov Sh.O'</span>
+         <section class="text-right font-semibold">
+            Kadrlar bo'limi boshlig'i
+            <br />
+            A.B.Butayev
+         </section>
+      </div>
+      <div class="flex justify-end mt-2">
+         <span> _____________________ </span>
+      </div>
+      <div class="flex justify-end mt-4">
+         <span> «____»_____________ y. </span>
+      </div>
+      <div class="flex justify-start my-4">
+         <span> Asos: </span>
+      </div>
+      <nav class="text-center text-[12pt] uppercase font-bold tracking-[4px] mb-1">Qabul varaqasi</nav>
+      <table class="w-full">
+         <tbody>
+            <tr>
+               <td class="text-[11pt] align-bottom">Familiya, ismi, sharifi</td>
+               <td class="text-[13pt] align-bottom border-b px-4 py-0.5">
+                  Palonchayev Pistoncha Pistonchaevich
+               </td>
+            </tr>
+            <tr>
+               <td class="text-[11pt] align-bottom">Ish joyi</td>
+               <td class="text-[13pt] align-bottom border-b px-4 py-0.5">
+                  3-sonli Gidrometallurgiya zavodi , Maydalash tsexi
+               </td>
+            </tr>
+            <tr>
+               <td class="text-[11pt] align-bottom">Kasbi (lavozimi)</td>
+               <td class="text-[13pt] align-bottom border-b px-4 py-0.5">Sex boshlig‘i</td>
+            </tr>
+            <tr>
+               <td class="text-[11pt] align-bottom">Razryadi (oklad)</td>
+               <td class="text-[13pt] align-bottom border-b p-4"></td>
+            </tr>
+         </tbody>
+      </table>
+      <nav class="text-center text-[12pt] uppercase font-bold tracking-[4px] mb-1">Yo'riqnomalar</nav>
+      <table class="text-[10pt]">
+         <tbody>
+            <tr class="text-center leading-3">
+               <td class="border py-0.5 min-w-8">№</td>
+               <td class="border py-0.5 w-full">Yo'riqnoma o'tiladigan joylar</td>
+               <td class="border py-0.5 min-w-26">Yo'riqnoma o'tkazilgan kun</td>
+               <td class="border py-0.5 min-w-26">Yo'riqnoma o'tqazuvchi</td>
+               <td class="border py-0.5 min-w-26">
+                  Yo'riqnoma o'tqazuvchi <br />
+                  imzosi
+               </td>
+               <td class="border py-0.5 min-w-26">
+                  Yo'riqnoma o'tuvchi <br />
+                  imzosi
+               </td>
+            </tr>
+         </tbody>
+      </table>
    </div>
 </template>
 
